e0f36c11f7e2c56a856d707a6820a16d4eedf6c1
eniiazov <esenniiazov@gmail.com>
2023-03-16 10:02:43 -0500

    adding welcome method to Google

--- src/test/java/pages/Google.java
+++ src/test/java/pages/Google.java
@@ -5,4 +5,8 @@ public class Google {
     public static void main(String[] args) {
         System.out.println("Welcome to Google!");
     }
+
+    public void welcome(){
+        System.out.println("Welcome to the city!");
+    }
 }
